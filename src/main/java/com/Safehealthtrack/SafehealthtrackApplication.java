9cd49ecc5eca2422ee90ff3a3f634f2b25698085
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2025-01-08 13:00:59 -0600

    first commit

---
+++ src/main/java/com/Safehealthtrack/SafehealthtrackApplication.java
@@ -0,0 +1,13 @@
+package com.Safehealthtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafehealthtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafehealthtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Safehealthtrack/SafehealthtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Safehealthtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafehealthtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
